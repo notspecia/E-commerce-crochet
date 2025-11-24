547ae68f35812b172b1e7c0bb98999c6515ecfa7
Gabriele Speciale <gabspec@gmail.com>
2025-11-24 22:08:12 +0100

    feat: partial rework of checkout page success after payment

--- src/views/SuccessPayment/SuccessPayment.vue
+++ src/views/SuccessPayment/SuccessPayment.vue
@@ -4,21 +4,16 @@
 
 
 <template>
-    <div class="success-container container d-flex flex-column align-items-center justify-content-center text-center">
-
+    <div class="conatiner-success-payment">
         <!-- Immagine centrale -->
-        <img class="success-image mb-4"
-            src="https://hips.hearstapps.com/clv.h-cdn.co/assets/16/18/gettyimages-586890581.jpg?crop=0.668xw:1.00xh;0.219xw,0"
-            alt="Successo pagamento" />
-
+        <img class="success-image mb-4" src="/images/lucky-success.jpg" alt="Successo pagamento" />
         <!-- Messaggio di conferma -->
-        <h2 class="mb-3">Pagamento completato</h2>
-        <p class="lead description">
+        <h2 class="mb-2 fs-1">Pagamento completato</h2>
+        <p class="lead">
             Grazie per il tuo ordine. Riceverai aggiornamenti tramite email non appena l’ordine verrà elaborato.
         </p>
-
         <!-- Bottone per tornare alla Home -->
-        <router-link to="/" class="btn btn-primary mt-4">
+        <router-link to="/" class="btn btn-one mt-4">
             Torna alla Home
         </router-link>
     </div>
@@ -26,21 +21,20 @@
 
 
 <style scoped lang="scss">
-.success-container {
-    min-height: 70vh;
-    padding-top: 4rem;
+.conatiner-success-payment {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 
+    // Immagine di successo pagamento
     .success-image {
-        width: 180px;
-        height: 180px;
-        object-fit: contain;
-        opacity: 0.9;
-    }
+        width: 400px;
 
-    .description {
-        max-width: 420px;
-        margin: 0 auto;
-        color: #444;
+        @media (max-width: $breakpoint-lg) {
+            width: 250px;
+        }
     }
 }
 </style>
